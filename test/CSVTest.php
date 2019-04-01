9f75ec36826b61779e194ade61f2894af7973ec9
zalakpatel <zp55@njit.edu>
2019-03-31 21:03:28 -0400

    Composer.lock

--- test/CSVTest.php
+++ test/CSVTest.php
@@ -1,6 +1,8 @@
 <?php
+declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
 
-class CSVTest extends \PHPUnit_Framework_TestCase
+class CSVTest extends TestCase
 {
     protected $filepath;
     protected $csv_file;
